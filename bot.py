db9889d04c882cdaea0e3f94e64065a3696f5b23
tima669 <timafilipcuk98@gmail.com>
2026-05-21 09:39:09 +0300

    Add new token

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 
 # Сюди вставляємо свій токен з BotFather
-API_TOKEN = "8746185237:AAF2QJAfqg9eu0qODEfhNZ39pVwxzFsWLJU"
+API_TOKEN = "8746185237:AAHu0oguurkr_oK6_dM_Ge7KLJL3wKcvnf8"
 
 # Створюємо бота і диспетчер
 bot = Bot(token=API_TOKEN)
@@ -14,7 +14,12 @@ dp = Dispatcher()
 # Обробник команди /start
 @dp.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer("Привіт! Я твій перший бот!")
+    await message.answer("Я бот який повторюю")
+
+# Обробник усіх повідомлень
+@dp.message()
+async def repeat_handler(message: Message):
+    await message.answer(message.text)
 
 # Головна функція для запуску бота
 async def main():
